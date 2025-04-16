0f98ea72434fd83a00d7e01be6de9ce0cb477d19
FyascOo <alan.choufa@gmail.com>
2025-04-16 22:42:27 +0200

    addtailwind + material icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,20 @@ import { RouterModule } from '@angular/router';
 @Component({
   imports: [RouterModule],
   selector: 'app-root',
-  template: ` <router-outlet></router-outlet>`,
+  template: ` <div class="navbar bg-base-100 shadow-sm">
+    <div class="flex-none">
+      <button class="btn btn-square btn-ghost">
+        <span class="material-icons"> menu </span>
+      </button>
+    </div>
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">MyLittleGarden</a>
+    </div>
+    <div class="flex-none">
+      <button class="btn btn-square btn-ghost">
+        <span class="material-icons"> more_horiz </span>
+      </button>
+    </div>
+  </div>`,
 })
 export class AppComponent {}
